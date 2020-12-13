ffc58b7dd34db22ed66ebc3ae0ea25fbb91a5c04
jc8080 <jc8080@rit.edu>
2020-12-13 13:55:45 -0500

    Able to add const courses upon initial DOM creation

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 // Course class: Represent a Course
 
+class Courses {
+    constructor(name, ID, credit){
+        this.name = name;
+        this.IDs = ID;
+        this.credit = credit;
+    }
+}
 // UI Class: Handle UI Tasks
+class UI{
+    static displayCourses(){
+        const storedCourses = [
+            {
+                name: 'Computer Science I ',
+                IDs: 'CS-101',
+                credit: 4
+            },
+            {
+                name: 'Computer Science II ',
+                IDs: 'CS-102',
+                credit: 4
+            }
+        ];
+
+        const courses = storedCourses;
+
+        courses.forEach((courses) => UI.addCourseToList(courses));
+    }
+
+    static addCourseToList(courses) {
+        const list = document.querySelector('#courses-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <th>${courses.name}</th>
+        <th>${courses.IDs}</th>
+        <th>${courses.credit}</th>
+        <th><button>delete</button></th>
+        `;
+
+        list.appendChild(row);
+    }
+}
 
 // Store Class: Handles Storage
 
 // Event: Display Courses
+document.addEventListener('DOMContentLoaded', UI.displayCourses);
 
 // Event: Add a Course
 
