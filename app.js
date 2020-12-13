e61552f12a66727e0b0ae6267f6235e875db61d0
jc8080 <jc8080@rit.edu>
2020-12-13 13:08:31 -0500

    Implemented basic skeleton for html and css. Ready for javascript implementation

---
+++ app.js
@@ -0,0 +1,11 @@
+// Course class: Represent a Course
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display Courses
+
+// Event: Add a Course
+
+// Event: Remove a Course
